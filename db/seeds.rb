6aeb88de74d936a1884c28c343e656b5b6b17f05
Valantina Tsoi <99823443+valantinatsoi@users.noreply.github.com>
2022-06-05 14:53:43 +0800

    pets routes and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'date'
 
+Pet.all.destroy
+
 Pet.create(name: "Tokaji", address: "Zhenning Road", species: "dog", found_on: Date.today)
 Pet.create(name: "Lady", address: "Yuyuan Road", species: "dog", found_on: Date.yesterday)
 Pet.create(name: "Bunny", address: "Yongjia Road", species: "rabbit", found_on: Date.tomorrow)
